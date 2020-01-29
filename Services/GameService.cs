9a1880a9691d27bab415c78f2b3a0abea6742616
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2020-01-29 10:45:37 -0700

    Rooney

--- Services/GameService.cs
+++ Services/GameService.cs
@@ -1,7 +1,22 @@
+using System.Collections.Generic;
+using FedUp.Models;
+
 namespace FedUp.Services
 {
     class GameService
     {
+        private Game Game { get; set; } = new Game();
+        public List<string> Messages { get; set; } = new List<string>();
 
+        public void PrintMenu()
+        {
+            Messages.Add($"Welcome to {Game.CurrentAirport.Name}");
+            Messages.Add("Destinations");
+            foreach (var des in Game.CurrentAirport.Destinations)
+            {
+                Messages.Add($"{des.Key} -- {des.Value.Name}");
+            }
+            Messages.Add("Where to go?");
+        }
     }
 }
