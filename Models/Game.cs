5ff96ac44ff1114ecdc70f892ca507c5cd8d7100
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2020-01-29 10:21:17 -0700

    Bugs

--- Models/Game.cs
+++ Models/Game.cs
@@ -2,6 +2,23 @@ namespace FedUp.Models
 {
     class Game
     {
+        public void Setup()
+        {
+            //Create Data 
+            Airport Boise = new Airport("Boise", "BOI");
+            Airport Denver = new Airport("Denver", "DEN");
+            Airport LosAngeles = new Airport("Los Angeles", "LAX");
+            Airport Portland = new Airport("Portland", "PDX");
+            Airport Phoenix = new Airport("Phoenix", "PHX");
+            Airport NewYork = new Airport("New York", "JFK");
+            Airport Miami = new Airport("Miami", "MIA");
+            Airport Guadalajara = new Airport("Guadalajara", "GDL");
+            Airport Honolulu = new Airport("Honolulu", "HNL");
+            Airport Dallas = new Airport("Dallas", "DFW");
 
+            // Create Relationships
+
+            Boise.Destinations.Add(Denver.Code, Denver);
+        }
     }
 }
